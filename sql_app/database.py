0151939aefc071cace1ba1e00a76bc3212bdf26e
xabados <xabados@aceproject.space>
2023-03-13 13:27:55 -0500

    edited db url

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://gryd_dev:password@localhost/gryd_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://gryd_dev:password@localhost/gryd_db?charset=utf8mb4"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
