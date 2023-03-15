98ec6ebfaa68a76c07dd821d7ff60dc3970bf9d1
xabados <xabados@aceproject.space>
2023-03-15 13:32:04 -0500

    added get_zones function

--- sql_app/read.py
+++ sql_app/read.py
@@ -4,3 +4,6 @@ from . import models, schemas
 
 def get_spots(db: Session, skip: int = 0):
     return db.query(models.Spots).offset(skip).all()
+
+def get_zones(db: Session, skip: int = 0):
+    return db.query(models.Zones).offset(skip).all()
