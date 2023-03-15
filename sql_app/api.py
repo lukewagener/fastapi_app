487af785d226d60af5921880d474ade6cb28ef2c
xabados <xabados@aceproject.space>
2023-03-15 15:10:34 -0500

    added api route for get_zones_by_spot function

--- sql_app/api.py
+++ sql_app/api.py
@@ -20,7 +20,14 @@ def read_zones(skip: int = 0, db: Session = Depends(get_db)):
     zones = read.get_zones(db, skip=skip)
     return zones
 
+# FrontEnd Default View (Pins on the Map)
 @app.get("/spots/", response_model=list[schemas.Spots])
 def read_spots(skip: int = 0, db: Session = Depends(get_db)):
     spots = read.get_spots(db, skip=skip)
     return spots
+
+# FrontEnd Selected Pin on the Map
+@app.get("/zones/{zoneGuid}", response_model=list[schemas.Zones])
+def read_zone(zoneGuid: str, db: Session = Depends(get_db)):
+    db_zone = read.get_zones_by_spot(db, zoneGuid=zoneGuid)
+    return db_zone
