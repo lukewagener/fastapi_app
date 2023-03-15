0a1e3ae5b61a3a84e26c6a2aac0fb681ed7d11e7
xabados <xabados@aceproject.space>
2023-03-15 11:00:40 -0500

    renamed function

--- sql_app/read.py
+++ sql_app/read.py
@@ -2,5 +2,5 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
-def get_properties(db: Session, skip: int = 0):
-    return db.query(models.Properties).offset(skip).all()
+def get_spots(db: Session, skip: int = 0):
+    return db.query(models.Spots).offset(skip).all()
