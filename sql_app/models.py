df0bfbe320621af034906ca44082e6eeef37ddde
xabados <xabados@aceproject.space>
2023-03-15 11:09:02 -0500

    renamed & restructured db  models

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,42 +1,40 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Decimal
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Numeric
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
 
-class Properties(Base):
-    __tablename__ = "properties"
+class Spots(Base):
+    __tablename__ = "spots"
 
-    propertyGuid = Column("PropertyGuid", String)
-    # zoneGuid = Column("ZoneGuid", String, ForeignKey("zones.zoneGuid"))
-    propertyName = Column("PropertyName", String)
-    zoneName = Column("ZoneName", String)
-    address = Column("Address", String)
-    postalCode = Column("Postal code", String)
-    latitude = Column("Latitude", Decimal)
-    longitude = Column("Longitude", Decimal)
-    coveredParking = Column("Covered Parking", Boolean)
-    electricCharger = Column("Electric charger for EV", Boolean) 
-    blockHeater = Column("Block Heater", Boolean)
-    rating = Column("Rating", Decimal)
-    reservationCount = Column("ReservationCount", Integer)
-    reservedHours = Column("ReservedHours", Integer)
-    spotCount = Column("SpotCount", Integer)
-
-    zoneGuid = relationship(Zones, back_populates="zoneGuid")
+    propertyGuid = Column(String, primary_key=True, index=True)
+    zoneGuid = Column(String, ForeignKey('zones.zoneGuid'))
+    propertyName = Column(String)
+    zoneName = Column(String)
+    address = Column(String)
+    postalCode = Column(String)
+    latitude = Column(Numeric)
+    longitude = Column(Numeric)
+    coveredParking = Column(Boolean)
+    electricCharger = Column(Boolean) 
+    blockHeater = Column(Boolean)
+    rating = Column(Numeric)
+    reservationCount = Column(Integer)
+    reservedHours = Column(Integer)
+    spotCount = Column(Integer)
 
 
 class Zones(Base):
     __tablename__ = "zones"
 
-    propertyName = Column("PropertyName", String)
-    # zoneGuid = Column("ZoneGuid", String)
-    zoneName = Column("ZoneName", String)
-    hourly = Column("Hourly", Decimal)
-    dailyRate = Column("Daily rate", Decimal)
-    eveningRate = Column("Evening rate", Integer)
-    fullRate = Column("24/7", Integer)
-    commuter = Column("Commuter", Integer)
-    eveningsWeekends = Column("Evenings & Weekends", Integer)
+    zoneGuid = Column(String, primary_key=True, index=True)
+    propertyName = Column(String)
+    zoneName = Column(String)
+    rateHourly = Column(Numeric)
+    rateDaily = Column(Numeric)
+    rateEvening = Column(Integer)
+    rateFull = Column(Integer)
+    commuter = Column(Integer)
+    eveningsWeekends = Column(Integer)
 
-    zoneGuid = relationship(Properties, back_populates="zones")
+    spots = relationship("Spots", backref="zones")
