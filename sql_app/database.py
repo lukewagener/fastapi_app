303ee88b4fcbeeaf20c725b2ddc3c6f869a766e1
xabados <xabados@aceproject.space>
2023-03-15 11:04:32 -0500

    restructured db url

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://gryd_dev:password@localhost/gryd_db?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://gryd_dev:password@localhost/gryd_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
