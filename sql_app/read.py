9b9c32afa7d4a3fab07aac4d5a1489c6b81c2542
xabados <xabados@aceproject.space>
2023-03-29 15:48:38 -0500

    added new query method that joins spots & zones tables

--- sql_app/read.py
+++ sql_app/read.py
@@ -14,6 +14,10 @@ def get_impark_spots(db: Session, skip: int = 0):
 def get_zones_by_spot(db: Session, zoneGuid: str):
     return db.query(models.Zones).filter(models.Zones.zoneGuid == zoneGuid).all()
 
+# Dynamic Pricing
+def get_prices_by_spot(db: Session, zoneGuid: str):
+    return db.query(models.Spots.coveredParking, models.Spots.electricCharger, models.Spots.rating, models.Spots.reservedHours, models.Spots.spotCount, models.Zones.rateDaily, models.Zones.rateEvening, models.Zones.rateFull).join(models.Spots, models.Spots.zoneGuid == models.Zones.zoneGuid).filter(models.Zones.zoneGuid == zoneGuid).all()
+
 
 # TESTING
 def get_zones(db: Session, skip: int = 0):
