db75c916a94d9a331ba63bbc6eb99acbee60e22f
Luke Wagener <lukewagener1@gmail.com>
2023-04-24 11:24:55 -0500

    Moved variables to .env for security

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# Load hidden environment variables
+load_dotenv()
 
 # Database URL
-URL = "mysql+pymysql://gryd_dev:password@localhost/gryd_db"
+URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
 # Database engine
 
